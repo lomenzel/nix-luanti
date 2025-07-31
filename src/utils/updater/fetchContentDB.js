5dbff146df1b8ea3fc5667f3e5dedde902e2097a
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2025-07-31 18:17:10 +0000

    refinements for fetch script

--- src/utils/updater/fetchContentDB.js
+++ src/utils/updater/fetchContentDB.js
@@ -4,7 +4,7 @@ const { exec } = require("child_process");
 
 const BASE_URL = "https://content.luanti.org/api/packages";
 const DB_FILE = path.join("generated", "./contentDB.json");
-const FETCH_LIMIT = 500;
+const FETCH_LIMIT = 1;
 
 async function fetchJSON(url) {
   return await (await fetch(url)).json()
@@ -25,25 +25,28 @@ async function updateAllPackages() {
   const localPackages = fs.existsSync(DB_FILE)
     ? JSON.parse(fs.readFileSync(DB_FILE, "utf-8"))
     : [];
-  const localPackageMap = new Map(
-    localPackages.map((pkg) => [`${pkg.name}, ${pkg.author}, ${pkg.release}`, pkg])
-  );
 
   const remotePackages = (await fetchJSON(BASE_URL))
     .filter((pkg) => pkg.release !== null)
-    .map(
-      (pkg) =>
-        localPackageMap.get(`${pkg.name}, ${pkg.author}, ${pkg.release}`) ?? pkg
-    );
+    .map((pkg) => {
+      return {
+        ...(localPackages[
+          pkg.type === "game" ? "games" : pkg.type === "mod" ? "mods" : "texturePacks"
+        ]?.[pkg.author]?.[pkg.name] ?? pkg), ...pkg
+      }
+    })
+    .sort(() => Math.random() - 0.5); // logs are more fun with random order
 
   console.log(
-    `Found ${remotePackages.length} remote packages, ${localPackages.length} local packages.`
+    `Found ${remotePackages.length} remote packages`
   );
 
   const packagesWithHash = remotePackages.filter((pkg) => pkg.hash);
   const packagesToUpdate = remotePackages
     .filter((pkg) => !pkg.hash)
     .slice(0, FETCH_LIMIT);
+
+  console.log(`${packagesWithHash.length} packages up to date, ${packagesToUpdate.length} will be updated`);
 
   const newlyHashedPackages = [];
   for (const pkg of packagesToUpdate) {
@@ -57,41 +60,62 @@ async function updateAllPackages() {
     details.dependencies = dependencies
 
     newlyHashedPackages.push({
-      author: details.author,
+      ...details,
       game_support: details.type === "mod"
         ? details.game_support
           .filter((game) => game.supports)
           .map((game) => ({ name: game.game.name, author: game.game.author }))
         : undefined,
-      name: details.name,
-      provides: details.provides,
-      release: details.release,
-      repo: details.repo,
-      short_description: details.short_description,
-      type: details.type,
       dependencies: details.type === "mod"
         ? details.dependencies[`${details.author}/${details.name}`]?.map(
           (dep) => dep.name
         ) ?? []
         : undefined,
+
       hash: hash,
     });
 
   }
 
-  // Combine the already processed packages with the newly hashed ones and sort.
-  const allPackages = [...packagesWithHash, ...newlyHashedPackages].sort((a, b) => {
-    if (a.name < b.name) return -1;
-    if (a.name > b.name) return 1;
-    if (a.author < b.author) return -1;
-    if (a.author > b.author) return 1;
-    return 0;
-  });
+  const allPackages = [...packagesWithHash, ...newlyHashedPackages].map(pkg => ({
+    author: pkg.author,
+    game_support: pkg.game_support.length == 0 ? undefined : pkg.game_support,
+    name: pkg.name,
+    provides: pkg.provides.length == 0 ? undefined : pkg.provides,
+    release: pkg.release,
+    repo: pkg.repo,
+    short_description: pkg.short_description,
+    type: pkg.type,
+    dependencies: pkg.dependencies.length == 0 ? undefined : pkg.dependencies,
+    hash: pkg.hash,
+  }));
+
+
+  structuredPackages = {
+    games: {},
+    mods: {},
+    texturePacks: {},
+  }
+
+  for (const pkg of allPackages) {
+    if (pkg.type === "game") {
+      structuredPackages.games[pkg.author] ??= {};
+      structuredPackages.games[pkg.author][pkg.name] = { ...pkg, author: undefined, type: undefined, name: undefined };
+    } else if (pkg.type === "mod") {
+      structuredPackages.mods[pkg.author] ??= {};
+      structuredPackages.mods[pkg.author][pkg.name] = { ...pkg, author: undefined, type: undefined, name: undefined };
+    } else if (pkg.type === "txp") {
+      structuredPackages.texturePacks[pkg.author] ??= {};
+      structuredPackages.texturePacks[pkg.author][pkg.name] = { ...pkg, author: undefined, type: undefined, name: undefined };
+    } else {
+      console.error(`Unknown package type: ${pkg.type} for ${pkg.author}/${pkg.name}`);
+    }
+  }
 
   if (!fs.existsSync(path.dirname(DB_FILE))) {
     fs.mkdirSync(path.dirname(DB_FILE), { recursive: true });
   }
-  fs.writeFileSync(DB_FILE, JSON.stringify(allPackages)); // Added formatting for readability
+  fs.writeFileSync(DB_FILE, JSON.stringify(structuredPackages));
   console.log("Package database updated successfully.");
 }
 updateAllPackages().catch(e => {
