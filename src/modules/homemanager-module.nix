0f61727fd456b44cca1f2bf559db3b25d695080c
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2025-08-01 16:39:21 +0000

    overlay hopefully in the right place

--- src/modules/homemanager-module.nix
+++ src/modules/homemanager-module.nix
@@ -16,79 +16,82 @@ let
 
 in
 {
-  nixpkgs.overlays = lib.singleton (import ../overlay.nix);
 
   inherit options;
-  config = lib.mkIf cfg.enable {
-    systemd.user.services =
-      builtins.mapAttrs
-        (
-          name: serverConfig:
-          let
-            whitelist = if serverConfig.whitelist == null then cfg.whitelist else serverConfig.whitelist;
-            mods =
-              serverConfig.mods
-              ++ (if whitelist == null then [ ] else [ pkgs.luantiPackages.mods.whitelist_by_AntumDeluge ]);
-            whitelistFile = pkgs.writeText "whitelist.txt" (builtins.concatStringsSep "\n" whitelist);
-            worldDir = "${config.xdg.dataHome}/nix-luanti/${name}/world";
-            minetestDir = "${config.xdg.configHome}/minetest";
-            gameDirName = "nix-luanti-${
-              builtins.replaceStrings
-                [
-                  "."
-                  "/"
-                  " "
-                ]
-                [
-                  "no"
-                  "no"
-                  "no"
-                ]
-                name
-            }";
-          in
-          {
-            Install.WantedBy = [ "default.target" ];
-            Unit.After = [ "network.target" ];
-            Unit.Description = "Luanti server instance for ${name}";
-            Service.ExecStart = pkgs.writeShellScript "start-${name}-server" ''
-              rm -rf ${worldDir}/whitelist.txt
-              rm -rf ~/.minetest/games/${gameDirName}
+  config = lib.mkMerge [
+    { nixpkgs.overlays = lib.singleton (import ../overlay.nix); }
 
-              mkdir -p ~/.minetest/games
-              mkdir -p ${worldDir}
-              ln -s ${serverConfig.game.withMods (m: mods)} ~/.minetest/games/${gameDirName}
-              ${
-                if whitelist == null then
-                  ""
-                else
-                  ''
-                    rm -rf ${worldDir}/whitelist.txt
-                    cat ${whitelistFile} > ${worldDir}/whitelist.txt
-                  ''
-              }
+    (lib.mkIf cfg.enable {
+      systemd.user.services =
+        builtins.mapAttrs
+          (
+            name: serverConfig:
+            let
+              whitelist = if serverConfig.whitelist == null then cfg.whitelist else serverConfig.whitelist;
+              mods =
+                serverConfig.mods
+                ++ (if whitelist == null then [ ] else [ pkgs.luantiPackages.mods.whitelist_by_AntumDeluge ]);
+              whitelistFile = pkgs.writeText "whitelist.txt" (builtins.concatStringsSep "\n" whitelist);
+              worldDir = "${config.xdg.dataHome}/nix-luanti/${name}/world";
+              minetestDir = "${config.xdg.configHome}/minetest";
+              gameDirName = "nix-luanti-${
+                builtins.replaceStrings
+                  [
+                    "."
+                    "/"
+                    " "
+                  ]
+                  [
+                    "no"
+                    "no"
+                    "no"
+                  ]
+                  name
+              }";
+            in
+            {
+              Install.WantedBy = [ "default.target" ];
+              Unit.After = [ "network.target" ];
+              Unit.Description = "Luanti server instance for ${name}";
+              Service.ExecStart = pkgs.writeShellScript "start-${name}-server" ''
+                rm -rf ${worldDir}/whitelist.txt
+                rm -rf ~/.minetest/games/${gameDirName}
 
-              ${serverConfig.package}/bin/luantiserver \
-                --config ${
-                  builtins.toFile "luanti.conf" (
-                    { prometheus_listener_address = "127.0.0.1:${toString serverConfig.port}"; } // serverConfig.config
-                    |> toConf
-                  )
-                } \
-                --port ${builtins.toString serverConfig.port} \
-                --color always \
-                --world ${worldDir} \
-                --gameid ${gameDirName}
-            '';
-          }
-        )
-        (
-          builtins.listToAttrs (
-            lib.mapAttrsToList (name: value: {
-              name = "luanti-${name}";
-              inherit value;
-            }) cfg.servers
+                mkdir -p ~/.minetest/games
+                mkdir -p ${worldDir}
+                ln -s ${serverConfig.game.withMods (m: mods)} ~/.minetest/games/${gameDirName}
+                ${
+                  if whitelist == null then
+                    ""
+                  else
+                    ''
+                      rm -rf ${worldDir}/whitelist.txt
+                      cat ${whitelistFile} > ${worldDir}/whitelist.txt
+                    ''
+                }
+
+                ${serverConfig.package}/bin/luantiserver \
+                  --config ${
+                    builtins.toFile "luanti.conf" (
+                      { prometheus_listener_address = "127.0.0.1:${toString serverConfig.port}"; } // serverConfig.config
+                      |> toConf
+                    )
+                  } \
+                  --port ${builtins.toString serverConfig.port} \
+                  --color always \
+                  --world ${worldDir} \
+                  --gameid ${gameDirName}
+              '';
+            }
           )
-        );
-  };
+          (
+            builtins.listToAttrs (
+              lib.mapAttrsToList (name: value: {
+                name = "luanti-${name}";
+                inherit value;
+              }) cfg.servers
+            )
+          );
+    })
+  ];
 }
--- src/modules/nixos-module.nix
+++ src/modules/nixos-module.nix
@@ -15,92 +15,93 @@ let
 in
 {
 
-  nixpkgs.overlays = lib.singleton (import ../overlay.nix);
-
   inherit options;
-  config = lib.mkIf cfg.enable {
-    users.groups.luanti = { };
-    users.users =
-      builtins.mapAttrs
-        (name: serverConfig: {
-          description = "User for Luanti Server ${builtins.replaceStrings [ "luanti" ] [ "" ] name}";
-          home = "/var/lib/${name}";
-          createHome = true;
-          group = "luanti";
-          isSystemUser = true;
-        })
-        (
-          builtins.listToAttrs (
-            lib.mapAttrsToList (name: value: {
-              name = "luanti-${name}";
-              inherit value;
-            }) cfg.servers
+  config = [
+    (lib.mkIf cfg.enable {
+      users.groups.luanti = { };
+      users.users =
+        builtins.mapAttrs
+          (name: serverConfig: {
+            description = "User for Luanti Server ${builtins.replaceStrings [ "luanti" ] [ "" ] name}";
+            home = "/var/lib/${name}";
+            createHome = true;
+            group = "luanti";
+            isSystemUser = true;
+          })
+          (
+            builtins.listToAttrs (
+              lib.mapAttrsToList (name: value: {
+                name = "luanti-${name}";
+                inherit value;
+              }) cfg.servers
+            )
+          );
+
+      systemd.services =
+        builtins.mapAttrs
+          (
+            name: serverConfig:
+            let
+              whitelist = if serverConfig.whitelist == null then cfg.whitelist else serverConfig.whitelist;
+              mods =
+                serverConfig.mods
+                ++ (if whitelist == null then [ ] else [ pkgs.luantiPackages.mods.whitelist_by_AntumDeluge ]);
+              whitelistFile = pkgs.writeText "whitelist.txt" (builtins.concatStringsSep "\n" whitelist);
+            in
+            {
+              #name = name;
+              description = "Luanti server instance for ${name}.";
+              after = [ "network.target" ];
+              wantedBy = [ "multi-user.target" ];
+              serviceConfig = {
+                ExecStart = pkgs.writeShellScript "start-luanti-server" ''
+                  rm -rf ~/.minetest
+                  rm -rf ~/world/whitelist.txt
+
+                  mkdir -p ~/.minetest/games
+                  mkdir -p ~/world
+
+                  ln -s ${serverConfig.game.withMods (m: mods)} ~/.minetest/games/${serverConfig.game.pname}
+                  ${
+                    if whitelist == null then
+                      ""
+                    else
+                      ''
+                        rm -rf ~/world/whitelist.txt
+                        cat ${whitelistFile} > ~/world/whitelist.txt
+                      ''
+                  }
+
+                  ${serverConfig.package}/bin/luantiserver \
+                    --config ${
+                      builtins.toFile "luanti.conf" (
+                        { prometheus_listener_address = "127.0.0.1:${toString serverConfig.port}"; } // serverConfig.config
+                        |> toConf
+                      )
+                    } \
+                    --port ${builtins.toString serverConfig.port} \
+                    --color always \
+                    --world ~/world \
+                    --gameid ${serverConfig.game.pname}
+                '';
+                # TODO: world generation should respect config, or does it by default?
+                User = name;
+                Group = "luanti";
+                Restart = "on-failure";
+
+              };
+            }
           )
-        );
+          (
+            builtins.listToAttrs (
+              lib.mapAttrsToList (name: value: {
+                name = "luanti-${name}";
+                inherit value;
+              }) cfg.servers
+            )
+          );
 
-    systemd.services =
-      builtins.mapAttrs
-        (
-          name: serverConfig:
-          let
-            whitelist = if serverConfig.whitelist == null then cfg.whitelist else serverConfig.whitelist;
-            mods =
-              serverConfig.mods
-              ++ (if whitelist == null then [ ] else [ pkgs.luantiPackages.mods.whitelist_by_AntumDeluge ]);
-            whitelistFile = pkgs.writeText "whitelist.txt" (builtins.concatStringsSep "\n" whitelist);
-          in
-          {
-            #name = name;
-            description = "Luanti server instance for ${name}.";
-            after = [ "network.target" ];
-            wantedBy = [ "multi-user.target" ];
-            serviceConfig = {
-              ExecStart = pkgs.writeShellScript "start-luanti-server" ''
-                rm -rf ~/.minetest
-                rm -rf ~/world/whitelist.txt
-
-                mkdir -p ~/.minetest/games
-                mkdir -p ~/world
-
-                ln -s ${serverConfig.game.withMods (m: mods)} ~/.minetest/games/${serverConfig.game.pname}
-                ${
-                  if whitelist == null then
-                    ""
-                  else
-                    ''
-                      rm -rf ~/world/whitelist.txt
-                      cat ${whitelistFile} > ~/world/whitelist.txt
-                    ''
-                }
-
-                ${serverConfig.package}/bin/luantiserver \
-                  --config ${
-                    builtins.toFile "luanti.conf" (
-                      { prometheus_listener_address = "127.0.0.1:${toString serverConfig.port}"; } // serverConfig.config
-                      |> toConf
-                    )
-                  } \
-                  --port ${builtins.toString serverConfig.port} \
-                  --color always \
-                  --world ~/world \
-                  --gameid ${serverConfig.game.pname}
-              '';
-              # TODO: world generation should respect config, or does it by default?
-              User = name;
-              Group = "luanti";
-              Restart = "on-failure";
-
-            };
-          }
-        )
-        (
-          builtins.listToAttrs (
-            lib.mapAttrsToList (name: value: {
-              name = "luanti-${name}";
-              inherit value;
-            }) cfg.servers
-          )
-        );
-
-  };
+    })
+    { nixpkgs.overlays = lib.singleton (import ../overlay.nix); }
+  ];
 }
