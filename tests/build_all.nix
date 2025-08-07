f370a35221dce99f870cca3f71324dc852b828fd
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2025-08-07 09:26:49 +0000

    remove build all test

--- tests/build_all.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }:
-pkgs.luanti.withPackages {
-  texturePacks = builtins.attrValues pkgs.luantiPackages.texturePacks;
-  mods = builtins.attrValues pkgs.luantiPackages.mods;
-  games = builtins.attrValues pkgs.luantiPackages.games;
-}
