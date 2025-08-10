55ed3d740fa4becb84f3996e02b7f6e0f9d3277c
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2025-08-10 20:51:13 +0000

    toomuchinfo: init

---
+++ src/clientMods/toomuchinfo/default.nix
@@ -0,0 +1,8 @@
+{ fetchFromGitHub }:
+fetchFromGitHub {
+  name = "toomuchinfo";
+  owner = "SwissalpS";
+  repo = "CSMtooMuchInformationHUD";
+  rev = "29b4ba38ca07d28f91b379c191b53ab37957aa98";
+  hash = "sha256-jU498UNRm2KkdwJn0K9LoWzjcetyOyW4tqDOC/pxvZw=";
+}
