dafed81c64efe8ade6648859fd757efa7a6cdc56
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2025-08-10 20:39:21 +0000

    alarm_watch: init at 1.0.1

---
+++ src/clientMods/alarm_watch/default.nix
@@ -0,0 +1,8 @@
+{ fetchFromGitHub }:
+fetchFromGitHub {
+  name = "alarm_watch";
+  owner = "HubTou";
+  repo = "alarm_watch";
+  tag = "1.0.1";
+  hash = "sha256-Vfp2X8koBeXO8v7Tx2kFTf5h8g4as714zZhofl/1P/s=";
+}
