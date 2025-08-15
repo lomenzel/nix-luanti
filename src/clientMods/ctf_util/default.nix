42320b5455ffbb5016707447fb05ea6d2c669344
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2025-08-15 12:42:36 +0000

    ctf_util: init

---
+++ src/clientMods/ctf_util/default.nix
@@ -0,0 +1,7 @@
+{ fetchFromGitHub }: fetchFromGitHub {
+  name = "ctf_util";
+  repo = "CTF-Util";
+  owner = "Infernia829";
+  rev = "1912f7f7fb375b31344768e1254efa62695b3109";
+  hash  = "sha256-luUMgidIN4wCqPfhpLqPHmx4qc0h517bQ/v49DLozHE=";
+}
