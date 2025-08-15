ba475e24239e14019281932b4045956f64946373
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2025-08-15 10:41:41 +0000

    ignore: init at v1.0.0-rc1

---
+++ src/clientMods/ignore/default.nix
@@ -0,0 +1,7 @@
+{fetchFromGitLab}: fetchFromGitLab {
+  name = "ignore";
+  owner = "jkoci";
+  repo = "minetest_csm_ignore";
+  rev = "156c0e2dc7a0e72ee6b603347258e3e2e14b407b";
+  hash = "sha256-zpN9TQgroLQfHBdp36tXFjO6m6t9DZp/RK2K4H0VENA=";
+}
