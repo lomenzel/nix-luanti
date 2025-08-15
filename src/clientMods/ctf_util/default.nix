00722af9881972c0464a3653647e3d105cf4b140
CI Pipeline <ci-pipeline@menzel.lol>
2025-08-15 12:43:42 +0000

    update and format

--- src/clientMods/ctf_util/default.nix
+++ src/clientMods/ctf_util/default.nix
@@ -1,7 +1,8 @@
-{ fetchFromGitHub }: fetchFromGitHub {
+{ fetchFromGitHub }:
+fetchFromGitHub {
   name = "ctf_util";
   repo = "CTF-Util";
   owner = "Infernia829";
   rev = "1912f7f7fb375b31344768e1254efa62695b3109";
-  hash  = "sha256-luUMgidIN4wCqPfhpLqPHmx4qc0h517bQ/v49DLozHE=";
+  hash = "sha256-luUMgidIN4wCqPfhpLqPHmx4qc0h517bQ/v49DLozHE=";
 }
