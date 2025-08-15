16cacb5ef6719a1529d55d726dc26538f0fd965e
CI Pipeline <ci-pipeline@menzel.lol>
2025-08-15 10:21:19 +0000

    update and format

--- src/clientMods/area_plotter/default.nix
+++ src/clientMods/area_plotter/default.nix
@@ -1,12 +1,15 @@
-{ fetchgit, writeText, stdenv }:let 
-  src =
-fetchgit {
-  url = "https://notabug.org/Maverick2797/area_plotter";
-  rev  = "c675e84bfce622b37d2174ae635c6f15049d9e4e";
-  name = "area_plotter_source";
-  outputHash = "sha256-aM2x6X3jRvfH4DLlG91S7vRjJLlUPXnu92RRXRg4V2U=";
-}
-;
+{
+  fetchgit,
+  writeText,
+  stdenv,
+}:
+let
+  src = fetchgit {
+    url = "https://notabug.org/Maverick2797/area_plotter";
+    rev = "c675e84bfce622b37d2174ae635c6f15049d9e4e";
+    name = "area_plotter_source";
+    outputHash = "sha256-aM2x6X3jRvfH4DLlG91S7vRjJLlUPXnu92RRXRg4V2U=";
+  };
 in
 
 stdenv.mkDerivation {
